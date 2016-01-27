d76dfafd6e65deadff3667fd74c80686265b1109
Gregor von Laszewski <laszewski@gmail.com>
2016-01-27 14:03:30 -0500

    chg: dev: using prompt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,8 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
 ]
-
+extensions += ['sphinx-prompt']
+    
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
